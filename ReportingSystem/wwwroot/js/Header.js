0618723fd16b9b78d4275eb76bbcf68716c16f7d
Shevelski <shevelski@gmail.com>
2023-09-24 20:40:28 +0300

    First Secon initials

--- ReportingSystem/wwwroot/js/Header.js
+++ ReportingSystem/wwwroot/js/Header.js
@@ -1,23 +1,58 @@
 new Vue({
     el: '#header_dropdown',
+    
     data() {
       return {
         windowHeight: window.innerHeight,
-        windowWidth: window.innerWidth
+        windowWidth: window.innerWidth,
+        mode: 'standart',
+        personalInfo: [0],
+        lengthArray: 0,
+        modalTitle: '',
+        dateArray: [],
+        holidayDays: 0,
+        hospitalDays: 0,
+        assignmentDays: 0,
+        taketimeoffDays: 0,
+        modalEmployeeActive: false,
+        modalOperation: null,
+        modalType: 0,
+        firstInitial: "",
+        secondInitial: ""
       }
     },
-  
     mounted() {
       this.$nextTick(() => {
         window.addEventListener('resize', this.onResize);
-      })
+      });
+      this.customerId = document.getElementById('idCu').textContent;
+      this.companyId = document.getElementById('idCo').textContent;
+      this.employeeId = document.getElementById('idEm').textContent;
+      this.rol = document.getElementById('rol').textContent;
+      this.Init();
     },
-  
     beforeDestroy() { 
       window.removeEventListener('resize', this.onResize); 
     },
-  
     methods: {  
+        async Init() {
+        this.personalInfo = await this.getEmployee();
+        this.firstInitial = this.personalInfo.firstName[0];
+        this.secondInitial = this.personalInfo.secondName[0];
+        console.log(this.firstInitial);
+        console.log(this.secondInitial);
+    },
+  
+    async getEmployee() {
+        let response = await axios.get("/Employees/GetEmployee", {
+            params: {
+                idCu: this.selectedCustomerId,
+                idCo: this.selectedCompanyId,
+                idEm: this.employeeId
+            }
+        });
+        return response.data;
+    },
       onResize() {
         this.windowHeight = window.innerHeight
         this.windowWidth = window.innerWidth
