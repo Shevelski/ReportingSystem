07b4db6535a42ec675ae3f85f9d4cb891469a24d
Serhii Naku~ <naku.serega@gmail.com>
2024-02-14 09:07:37 +0200

    developer bootstrap

--- ReportingSystem/wwwroot/js/Layout.js
+++ ReportingSystem/wwwroot/js/Layout.js
@@ -1,75 +1,10 @@
-﻿//$(document).ready(function () {
-//    function loadContent(url, target) {
-//        $.ajax({
-//            url: url,
-//            type: 'GET',
-//            success: function (data) {
-
-//                $(target).empty();
-//                $(target).html(data);
-//            },
-//            error: function () {
-//                alert('Error loading content');
-//            }
-//        });
-//    }
-
-//    $('.nav-link').on('click', function (e) {
-//        e.preventDefault();
-//    var url = $(this).attr('href');
-//    var target = $(this).data('target');
-//    loadContent(url, target);
-//    });
-
-
-//    $('.navbar-brand').on('click', function (e) {
-//        e.preventDefault();
-//    var url = $(this).attr('href');
-//    var target = $(this).data('target');
-//    loadContent(url, target);
-//        });
-
-//    $('.linkfooter').on('click', function (e) {
-//        e.preventDefault();
-//    var url = $(this).attr('href');
-//    var target = $(this).data('target');
-//    loadContent(url, target);
-//    if ($('.navbar-brand').hasClass('active')) {
-//        $('.navbar-brand').removeClass('active');
-//            }
-//    if ($('.nav-link').hasClass('active')) {
-//        $('.nav-link').removeClass('active');
-//            }
-//        });
-
-//    $(document).ready(function () {
-//        $('.nav-link').click(function () {
-//            if ($('.navbar-brand').hasClass('active')) {
-//                $('.navbar-brand').removeClass('active');
-//            }
-//            $('.nav-link').removeClass('active');
-//            $(this).closest('.nav-link').addClass('active');
-//        });
-//        });
-
-//    $(document).ready(function () {
-//        $('.navbar-brand').click(function () {
-//            if ($('.nav-link').hasClass('active')) {
-//                $('.nav-link').removeClass('active');
-//            }
-//            $(this).closest('.navbar-brand').addClass('active');
-//        });
-//    });
-//});
-
-<script>
-    $(document).ready(function () {
+﻿    $(document).ready(function () {
+        // Function to load content dynamically
         function loadContent(url, target) {
             $.ajax({
                 url: url,
                 type: 'GET',
                 success: function (data) {
-                    $(target).empty();
                     $(target).html(data);
                 },
                 error: function () {
@@ -78,46 +13,18 @@
             });
         }
 
-        $('.nav-link').on('click', function (e) {
+            // Handle navigation clicks
+            $('.nav-link').on('click', function (e) {
         e.preventDefault();
     var url = $(this).attr('href');
     var target = $(this).data('target');
     loadContent(url, target);
-        });
+            });
 
-    $('.navbar-brand').on('click', function (e) {
-        e.preventDefault();
-    var url = $(this).attr('href');
-    var target = $(this).data('target');
-    loadContent(url, target);
+    $(document).ready(function () {
+        $('.nav-link').click(function () {
+            $('.nav-link').removeClass('active'); // Remove 'active' class from all items
+            $(this).closest('.nav-link').addClass('active'); // Add 'active' class to the clicked item
         });
-
-    $('.linkfooter').on('click', function (e) {
-        e.preventDefault();
-    var url = $(this).attr('href');
-    var target = $(this).data('target');
-    loadContent(url, target);
-    if ($('.navbar-brand').hasClass('active')) {
-        $('.navbar-brand').removeClass('active');
-            }
-    if ($('.nav-link').hasClass('active')) {
-        $('.nav-link').removeClass('active');
-            }
+            });
         });
-
-    $('.nav-link').click(function () {
-            if ($('.navbar-brand').hasClass('active')) {
-        $('.navbar-brand').removeClass('active');
-            }
-    $('.nav-link').removeClass('active');
-    $(this).closest('.nav-link').addClass('active');
-        });
-
-    $('.navbar-brand').click(function () {
-            if ($('.nav-link').hasClass('active')) {
-        $('.nav-link').removeClass('active');
-            }
-    $(this).closest('.navbar-brand').addClass('active');
-        });
-    });
-</script>
