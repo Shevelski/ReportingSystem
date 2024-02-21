2f33a94db63fcf013581f219cae429a5a76d4dd8
shevelski <a.shevelski@gmail.com>
2024-02-21 16:00:12 +0200

    About us + clean code

--- ReportingSystem/wwwroot/js/Layout.js
+++ ReportingSystem/wwwroot/js/Layout.js
@@ -1,10 +1,14 @@
 ﻿$(document).ready(function () {
-        // Function to load content dynamically
+    // Function to load content dynamically
     function loadContent(url, target) {
         $.ajax({
             url: url,
             type: 'GET',
             success: function (data) {
+
+                $(target).empty();
+
+                // Load new content
                 $(target).html(data);
             },
             error: function () {
@@ -13,23 +17,15 @@
         });
     }
 
-        // Handle navigation clicks
+    // Handle navigation clicks
     $('.nav-link').on('click', function (e) {
         e.preventDefault();
         var url = $(this).attr('href');
         var target = $(this).data('target');
         loadContent(url, target);
-        });
-
-    $(document).ready(function () {
-        $('.nav-link').click(function () {
-            $('.nav-link').removeClass('active'); // Remove 'active' class from all items
-            $(this).closest('.nav-link').addClass('active'); // Add 'active' class to the clicked item
-        });
     });
 
 
-    // Handle navigation clicks
     $('.navbar-brand').on('click', function (e) {
         e.preventDefault();
         var url = $(this).attr('href');
@@ -37,12 +33,37 @@
         loadContent(url, target);
     });
 
+    $('.linkfooter').on('click', function (e) {
+        e.preventDefault();
+        var url = $(this).attr('href');
+        var target = $(this).data('target');
+        loadContent(url, target);
+        if ($('.navbar-brand').hasClass('active')) {
+            $('.navbar-brand').removeClass('active');
+        }
+        if ($('.nav-link').hasClass('active')) {
+            $('.nav-link').removeClass('active');
+        }
+    });
+
+
+    //Set active element in header
+    $(document).ready(function () {
+        $('.nav-link').click(function () {
+            if ($('.navbar-brand').hasClass('active')) {
+                $('.navbar-brand').removeClass('active');
+            }
+            $('.nav-link').removeClass('active');
+            $(this).closest('.nav-link').addClass('active');
+        });
+    });
+
     $(document).ready(function () {
         $('.navbar-brand').click(function () {
-            $('.navbar-brand').removeClass('active'); // Remove 'active' class from all items
-            $(this).closest('.navbar-brand').addClass('active'); // Add 'active' class to the clicked item
+            if ($('.nav-link').hasClass('active')) {
+                $('.nav-link').removeClass('active');
+            }
+            $(this).closest('.navbar-brand').addClass('active');
         });
     });
 });
-
-
